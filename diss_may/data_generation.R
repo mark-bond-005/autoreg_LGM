fa0b2cc7d9ce3003e1b858bfd2776c65e5fdd894
Mark Bond <36740324+mark-bond-005@users.noreply.github.com>
2018-02-23 15:03:26 -0600

    Update data_generation.R

--- diss_may/data_generation.R
+++ diss_may/data_generation.R
@@ -1,3 +1,7 @@
+### We need data before we can estimate the model I am proposing.
+# This file generates appropriate data, using the variables nLGM and phi from master_file.R
+
+# The a and b parameters refer to the slope and difficulty of the items, respectively.
 bparms <- parameters[seq(1, 59, 2), "b"]
 aparms <- parameters[seq(1, 59, 2), "a"]
 nItems <- length(bparms)
@@ -7,6 +11,7 @@ growParms <- rtmvnorm(nLGM, mean=c(0, 0.34, phi),
                       sigma = rbind(c(1, 0, 0), c(0, 0.17, 0), c(0, 0, 0.273)),
                       lower= c(-Inf, -Inf, -1), upper = c(Inf, Inf, 1))
 
+# Here I generate initial ability, growth, and autoregression for each simulee. 
 theta_ <- growParms[ , 1]; beta1_ <- growParms[ , 2]; beta2_ <- growParms[ , 3]
 m0 <- rnorm(n=nLGM, sd=sqrt(occVar))
 #note m0 below
@@ -15,6 +20,7 @@ theta2 <- theta_ + 1*beta1_ + theta1*beta2_ + rnorm(n = nLGM, sd = sqrt(1-occVar
 theta3 <- theta_ + 2*beta1_ + theta2*beta2_ + rnorm(n = nLGM, sd = sqrt(1-occVar^2))
 theta4 <- theta_ + 3*beta1_ + theta3*beta2_ + rnorm(n = nLGM, sd = sqrt(1-occVar^2))
 
+# This function generates responses based on the model.
 genResp <- function(nLGM, nItems, theta, timePt){
   responses <- matrix(0, nLGM, nItems)
   for(i in 1:nLGM){
